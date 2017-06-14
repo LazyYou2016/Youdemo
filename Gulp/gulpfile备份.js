fee651976107a076e6307ed2d62b4aa7938955d1
LazyYou2016 <756642763@qq.com>
2017-06-14 11:41:43 +0800

    no message

--- Gulp/gulpfile备份.js
+++ Gulp/gulpfile备份.js
@@ -11,130 +11,115 @@ var gulp = require('gulp');
 	runSequence = require('gulp-sequence'),//执行顺序任务
 	htmlmin = require('gulp-htmlmin'),//html压缩
 	less = require('gulp-less');//less编译
-var docs_html = "docs/*.html",
-	docs_css = "docs/dist/css/*.css",
-	docs_cssmin = "docs/dist/css/*.min.css",
-	docs_cssmin_not = "!docs/dist/css/*.min.css",
-	docs_js = "docs/dist/js/*.js",
-	docs_jsmin = "docs/dist/js/*.min.js",
-	docs_jsmin_not = "!docs/dist/js/*.min.js",
-	docs_img = "docs/dist/imgs/**",
-	docs_font = "docs/dist/fonts/**",
-	docs_less = "docs/dist/css/*.less",
-	dist_del = "dist",
-	dist_html = "./",
-	dist_html_edit = "./*.html",
-	dist_css = "dist/css",
-	dist_js = "dist/js",
-	dist_img = "dist/imgs",
-	dist_font = "dist/fonts"
-	buildBasePath = 'dist/';//构建输出的目录
+
 //分离文件
 gulp.task('fileinclude', function() {
-    gulp.src(docs_html)
+    gulp.src('docs/**.html')
         .pipe(fileinclude({
 	        prefix: '@@',
 	        basepath: '@file'
         }))
-    .pipe(gulp.dest(dist_html));
+    .pipe(gulp.dest('./'));
 //  .pipe(notify({ message: 'head and foot task ok' }));	//提示
 });
 // 合并、压缩、重命名css
 gulp.task('minicss', function() {
-	return gulp.src([docs_css, docs_cssmin_not]) //路径
+	return gulp.src(['docs/dist/css/*.css', '!docs/dist/css/*.min.css']) //路径
 //		.pipe(concat('style.css'))	//合并css
 //	    .pipe(rename({ suffix: '.min' })) //重命名
 	    .pipe(minicss())	//压缩
-	    .pipe(gulp.dest(dist_css))	//输出文件夹
+	    .pipe(gulp.dest('dist/css'))	//输出文件夹
 //	    .pipe(notify({ message: 'mincss task ok' }));	//提示
 });
 //默认css
 gulp.task('css', function() {
-	return gulp.src(docs_css) //路径
-	    .pipe(gulp.dest(dist_css))	//输出文件夹
+	return gulp.src('docs/dist/css/*.css') //路径
+	    .pipe(gulp.dest('dist/css'))	//输出文件夹
 //	    .pipe(notify({ message: 'css task ok' }));	//提示
 });
 gulp.task('css-min', function() {
-	return gulp.src(docs_cssmin) //路径
-	    .pipe(gulp.dest(dist_css))	//输出文件夹
+	return gulp.src('docs/dist/css/*.min.css') //路径
+	    .pipe(gulp.dest('dist/css'))	//输出文件夹
 //	    .pipe(notify({ message: 'css task ok' }));	//提示
 });
 // 合并、压缩js文件
 gulp.task('minijs', function() {
-	return gulp.src([docs_js, docs_jsmin_not])
+	return gulp.src(['docs/dist/js/*.js', '!docs/dist/js/*.min.js'])
 //		.pipe(concat('common.js'))	//合并js
-//	    .pipe(rename({ suffix: '.min' }))
+	    .pipe(rename({ suffix: '.min' }))
 	    .pipe(minijs())
-	    .pipe(gulp.dest(dist_js))
+	    .pipe(gulp.dest('dist/js'))
 //	    .pipe(notify({ message: 'minijs task ok' }));
 });
 gulp.task('js', function() {
-	return gulp.src(docs_js)
-	    .pipe(gulp.dest(dist_js))
+	return gulp.src('docs/dist/js/*.js')
+	    .pipe(gulp.dest('dist/js'))
 //	    .pipe(notify({ message: 'js task ok' }));
 });
 gulp.task('js-min', function() {
-	return gulp.src(docs_jsmin)
-	    .pipe(gulp.dest(dist_js))
+	return gulp.src('docs/dist/js/*.min.js')
+	    .pipe(gulp.dest('dist/js'))
 });
 gulp.task('img', function() {
-	return gulp.src(docs_img)
-	    .pipe(gulp.dest(dist_img))
+	return gulp.src('docs/dist/imgs/**')
+	    .pipe(gulp.dest('dist/imgs'))
 //	    .pipe(notify({ message: 'imgs task ok' }));
 });
 gulp.task('fonts', function() {
-	return gulp.src(docs_font)
-	    .pipe(gulp.dest(dist_font))
+	return gulp.src('docs/dist/fonts/**')
+	    .pipe(gulp.dest('dist/fonts'))
 });
 gulp.task('less', function () {
-    gulp.src(docs_less)
+    gulp.src('docs/dist/css/*.less')
         .pipe(less())
         .pipe(minicss()) //兼容IE7及以下需设置compatibility属性 .pipe(cssmin({compatibility: 'ie7'}))
-        .pipe(gulp.dest(dist_css));
+        .pipe(gulp.dest('dist/css'));
 });
 
+
+var buildBasePath = 'dist/';//构建输出的目录
 
 //cssmd5，压缩后并用md5进行命名，下面使用revCollector进行路径替换
 gulp.task('minifycssmd5', function (){
-    gulp.src([docs_css, docs_cssmin_not])
+    gulp.src(['docs/dist/css/*.css', '!docs/dist/css/*.min.css'])
         //.pipe(concat('style.css'))//压缩后的css
         .pipe(minicss())//压缩css到一行
         .pipe(rev())//文件名加MD5后缀
-        .pipe(gulp.dest(dist_css))//输出到css目录
+        .pipe(gulp.dest(buildBasePath+'css'))//输出到css目录
         .pipe(rev.manifest('rev-css-manifest.json'))//生成一个rev-manifest.json
         .pipe(gulp.dest('rev'));//将 rev-manifest.json 保存到 rev 目录内
 });
 //imgmd5，压缩后并用md5进行命名，下面使用revCollector进行路径替换
 gulp.task('minifyimgmd5', function (){
-    gulp.src([docs_img])
+    gulp.src(['docs/dist/imgs/**'])
         .pipe(rev())//文件名加MD5后缀
-        .pipe(gulp.dest(dist_img))//输出到css目录
+        .pipe(gulp.dest(buildBasePath+'imgs'))//输出到css目录
         .pipe(rev.manifest('rev-img-manifest.json'))//生成一个rev-manifest.json
         .pipe(gulp.dest('rev'));//将 rev-manifest.json 保存到 rev 目录内
 });
 //jsmd5，压缩后并用md5进行命名，下面使用revCollector进行路径替换
 gulp.task('minifyjsmd5', function() {
-    gulp.src([docs_js, docs_jsmin_not])
+    gulp.src(['docs/dist/js/*.js', '!docs/dist/js/*.min.js'])
         //.pipe(concat('common.min.js'))//压缩后的js
         .pipe(minijs())//压缩js到一行
         .pipe(rev())//文件名加MD5后缀
-        .pipe(gulp.dest(dist_js))//输出到js目录
+        .pipe(gulp.dest(buildBasePath+'js'))//输出到js目录
         .pipe(rev.manifest('rev-js-manifest.json'))////生成一个rev-manifest.json
         .pipe(gulp.dest('rev'));//将 rev-manifest.json 保存到 rev 目录内
 });
 
 gulp.task('rev', function() {
     //html，针对js,css,img
-    gulp.src(['rev/*.json', dist_html_edit])
+    gulp.src(['rev/*.json', './*.html'])
         .pipe(revCollector({replaceReved:true }))
-        .pipe(gulp.dest(dist_html));
+        .pipe(gulp.dest('./'));
 }); 
 
 gulp.task('revimg', function() {
     //css，主要是针对img替换
-    gulp.src(['rev/rev-img-manifest.json',buildBasePath+'css/*.css'])
+    gulp.src(['rev/rev-img-manifest.json','dist/css/*.css'])
         .pipe(revCollector({replaceReved:true }))
-        .pipe(gulp.dest(dist_css));
+        .pipe(gulp.dest(buildBasePath+'css'));
 });
 
 
@@ -162,10 +147,10 @@ gulp.task('test', function (cb) {
 });
 
 gulp.task('del-all', function () {
-	return del([dist_del,dist_html_edit]);
+	return del(['dist','./*.html']);
 });
 gulp.task('del-dist', function () {
-	return del([dist_del]);
+	return del(['dist']);
 });
 //上线任务——带md5
 gulp.task('online-md5', function(cb) {  
@@ -189,9 +174,9 @@ gulp.task('minihtml', function () {
         minifyJS: true,//压缩页面JS
         minifyCSS: true//压缩页面CSS
     };
-    gulp.src(dist_html_edit)
+    gulp.src('*.html')
         .pipe(htmlmin(options))
-        .pipe(gulp.dest(dist_html));
+        .pipe(gulp.dest('./'));
 });
 
 
@@ -200,16 +185,17 @@ gulp.task('default', function(){
 	gulp.start('help');
 });
 
+//gulp.task('default', ['fileinclude']); 
 
-//gulp.task('modify', function(){
-//	gulp.run('fileinclude','minicss', 'css', 'minijs', 'js');
-//	gulp.watch('docs/dist/*.html', function(){
-//	    gulp.run('fileinclude');
-//	});
-//	gulp.watch(docs_css, ['minicss']);
-//	gulp.watch(docs_js, ['minijs']);
-//	gulp.watch(docs_js, ['js']);
-//});
+gulp.task('modify', function(){
+	gulp.run('fileinclude','minicss', 'css', 'minijs', 'js');
+	gulp.watch('docs/dist/*.html', function(){
+	    gulp.run('fileinclude');
+	});
+	gulp.watch('docs/dist/css/*.css', ['minicss']);
+	gulp.watch('docs/dist/js/*.js', ['minijs']);
+	gulp.watch('docs/dist/js/*.js', ['js']);
+});
 
 //帮助
 gulp.task('help',function () {
